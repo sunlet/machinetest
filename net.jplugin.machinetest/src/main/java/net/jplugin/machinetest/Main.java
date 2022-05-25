b97262b76efa542838643272f65703316a0c6bbc
sunlet <liuhang.163@163.com>
2022-05-25 14:54:10 +0800

    update version

--- net.jplugin.machinetest/src/main/java/net/jplugin/machinetest/Main.java
+++ net.jplugin.machinetest/src/main/java/net/jplugin/machinetest/Main.java
@@ -1,9 +1,10 @@
 package net.jplugin.machinetest;
 
+import net.jplugin.core.kernel.PluginApp;
 import net.jplugin.core.kernel.api.PluginEnvirement;
 
 public class Main {
 	public static void main(String[] args) {
-		PluginEnvirement.getInstance().startup();
+		PluginApp.main(null);
 	}
 }
