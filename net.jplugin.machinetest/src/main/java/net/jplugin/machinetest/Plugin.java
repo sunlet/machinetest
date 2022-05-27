7ff587febc272ebebfd7796775536f056482675f
sunlet <liuhang.163@163.com>
2022-05-27 12:11:49 +0800

    comment

--- net.jplugin.machinetest/src/main/java/net/jplugin/machinetest/Plugin.java
+++ net.jplugin.machinetest/src/main/java/net/jplugin/machinetest/Plugin.java
@@ -17,13 +17,17 @@ public class Plugin extends AbstractPlugin
 {
 	public static final String EP_TEST_ITEM="EP_TEST_ITEM";
 	public Plugin(){
-//		this.addExtensionPoint(ExtensionPoint.createList("net.jplugin.machinetest.api.ITestItem", ITestItem.class));
-//
-//		ExtensionMtHelper.addTestItemExtension(this, ComputeTest.class);
-//		ExtensionMtHelper.addTestItemExtension(this, NewObjectTest.class);
-//		ExtensionMtHelper.addTestItemExtension(this, DiskTest.class);
-//
-//		ExtensionServiceHelper.addServiceExtension(this,TestManager.class.getName(),TestManager.class);
+		/* 本项目通过标注实现，以下5行代码分别有四个类级别标注替代了，也可以去掉标注使用，取消以下代码注释，效果一样。
+
+		this.addExtensionPoint(ExtensionPoint.createList("net.jplugin.machinetest.api.ITestItem", ITestItem.class));
+
+		ExtensionMtHelper.addTestItemExtension(this, ComputeTest.class);
+		ExtensionMtHelper.addTestItemExtension(this, NewObjectTest.class);
+		ExtensionMtHelper.addTestItemExtension(this, DiskTest.class);
+
+		ExtensionServiceHelper.addServiceExtension(this,TestManager.class.getName(),TestManager.class);
+		*/
+
 	}
 
 	@RefService
