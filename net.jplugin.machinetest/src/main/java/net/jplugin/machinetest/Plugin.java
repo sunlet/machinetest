78c45300b5e788442aa2d4805bdf1fcc8a1cc7ba
sunlet <liuhang.163@163.com>
2022-05-27 12:15:02 +0800

    comment

--- net.jplugin.machinetest/src/main/java/net/jplugin/machinetest/Plugin.java
+++ net.jplugin.machinetest/src/main/java/net/jplugin/machinetest/Plugin.java
@@ -10,7 +10,8 @@ public class Plugin extends AbstractPlugin
 {
 	public static final String EP_TEST_ITEM="EP_TEST_ITEM";
 	public Plugin(){
-		/* 本项目通过标注实现，以下5行代码分别有四个类级别标注替代了，也可以去掉标注使用，取消以下代码注释，效果一样。
+		/* 本项目通过标注实现，以下5行代码分别有四个类级别标注(一个BindExtensionPoint，三个BindExtension，一个BindService)替代了，
+		也可以不使用标注，同时取消以下代码注释，效果一样。
 
 		this.addExtensionPoint(ExtensionPoint.createList("net.jplugin.machinetest.api.ITestItem", ITestItem.class));
 
